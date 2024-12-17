d3c0932e9da089919d69e780011250493a5ed801
Sreenand76 <sreenands93@gmail.com>
2024-12-17 09:29:46 -0800

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -75,7 +75,7 @@ const ProjectCard = ({ project }) => {
          
         </div>
         <div>
-          <div className='flex flex-col gap-3 p-1'>
+          <div className='flex flex-col gap-3 p-1 md:max-w-[40vw]'>
             <div className=' text-xl font-semibold flex gap-5 items-center'>
               {project.title}
               <a href={project.github_link}><BsGithub /></a>
